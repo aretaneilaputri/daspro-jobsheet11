af36354a20094f2e35f6304332e38d05b98d2658
“aretaneilaputri <retaaneila@gmail.com>
2025-11-19 21:41:08 +0700

    Modifikasi Percobaan 2 (Pertanyaan nomor 2)

--- BioskopWithScanner03.java
+++ BioskopWithScanner03.java
@@ -7,28 +7,62 @@ public class BioskopWithScanner03 {
 
         int baris, kolom;
         String nama, next;
+        int menu;
 
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+        do {
+            System.out.println("\n=== MENU BIOSKOP ===");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (menu) {
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                case 1:
+                    while (true) {
+                        System.out.print("Masukkan nama: ");
+                        nama = sc.nextLine();
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                        System.out.print("Masukkan baris: ");
+                        baris = sc.nextInt();
+
+                        System.out.print("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        sc.nextLine(); // buang enter
+
+                        penonton[baris - 1][kolom - 1] = nama;
+
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\n=== Daftar Penonton ===");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            System.out.print(penonton[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Keluar dari program...");
+                    break;
+
+                default:
+                    System.out.println("Menu tidak valid!");
             }
-        }
+
+        } while (menu != 3);
     }
 }
