da3d545105c72f5c0cce159ac1f486db6d2a06b1
“aretaneilaputri <retaaneila@gmail.com>
2025-11-20 16:22:24 +0700

    Modifikasi Percobaan 4 (Pertanyaan nomor 1)

--- SIAKAD03.java
+++ SIAKAD03.java
@@ -5,31 +5,33 @@ public class SIAKAD03 {
 
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah siswa   : ");
+        int jumlahSiswa = sc.nextInt();
 
-        // LANGKAH 6
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
-            double totalPerSiswa = 0;
+        System.out.print("Masukkan jumlah mata kuliah : ");
+        int jumlahMatkul = sc.nextInt();
 
-            for (int j = 0; j < nilai[i].length; j++) {
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
+            for (int j = 0; j < jumlahMatkul; j++) {
                 System.out.print("Nilai mata kuliah " + (j + 1) + " : ");
                 nilai[i][j] = sc.nextInt();
-                totalPerSiswa += nilai[i][j];
             }
         }
 
         System.out.println("\n===============================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah");
 
-        for (int j = 0; j < 3; j++) {       
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {   
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("Mata kuliah " + (j + 1) + " : " + (totalPerMatkul / 4));
+            System.out.println("Mata kuliah " + (j + 1) + " : " + (totalPerMatkul / jumlahSiswa));
         }
 
         sc.close();
